b21a0f9b88a0215d476ea80273cf7efd95a086ac
DinkytheCat <157430242+DinkytheCat@users.noreply.github.com>
2024-08-29 14:29:06 -0700

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,10 @@
 
+flowers = {"rose", "lily"}
+x = input("pick ")
+# if x not in flowers:
+#     print("nah")
+for word in x:
+    if word not in x:
+       print("nah")
+    else:
+        print("Okay")
