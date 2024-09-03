6dbcae830899720f2a9668a95c001877aadfc058
michealjordann <148394954+michealjordann@users.noreply.github.com>
2024-09-03 15:24:37 -0700

    Update turtlecode.py

--- turtlecode.py
+++ turtlecode.py
@@ -1,8 +1,22 @@
 import turtle as trtl
 t = trtl.Turtle()
+#position setup
 t.speed(100)
+t.penup()
+t.goto((0,-150))
+t.left(90)
+t.pensize(8)
+t.pendown()
 
+#stem and leaf
+t.pencolor("darkgreen")
+t.forward(125)
+#leaf
+t.right(45)
+t.stamp
 
 
 wn = trtl.Screen()
 wn.mainloop()
+
+#i did so much work ilya konichiwa!!!!
