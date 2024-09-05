6811463ddc87af36ee319297ed6229964aa5406d
michealjordann <148394954+michealjordann@users.noreply.github.com>
2024-09-05 12:57:37 -0700

    Update turtlecode.py

--- turtlecode.py
+++ turtlecode.py
@@ -1,5 +1,7 @@
 import turtle as trtl
 t = trtl.Turtle()
+#sunflower!!!
+t.speed(0.2)
 #position setup
 t.speed(100)
 t.penup()
@@ -12,11 +14,23 @@ t.pendown()
 t.pencolor("darkgreen")
 t.forward(125)
 #leaf
-t.right(45)
-t.stamp
+t.fillcolor('lightgreen')
+t.begin_fill()
+t.left(70)
+t.circle(100,43)
+t.left(120) 	
+t.circle(100,43)
+t.end_fill()
+t.goto((0,25))
+t.pencolor("saddlebrown")
+#flower petals
+t.fillcolor("saddlebrown")
+t.begin_fill()
+t.circle(30)
+t.end_fill()
+t.pencolor("yellow2")
 
 
 wn = trtl.Screen()
 wn.mainloop()
 
-#i did so much work ilya konichiwa!!!!
